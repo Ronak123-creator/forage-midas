13176f654cefabafdbe121b383a5d7e64933805a
ronak <ronaksinghkarki@gmail.com>
2025-11-03 10:34:03 +0545

    task 2

---
+++ src/main/java/com/jpmc/midascore/config/KafkaConsumerConfig.java
@@ -0,0 +1,46 @@
+package com.jpmc.midascore.config;
+
+
+import com.jpmc.midascore.foundation.Transaction;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@EnableKafka
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, Transaction> transactionConsumerFactory(){
+        Map<String,Object> props = new HashMap<>();
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        JsonDeserializer<Transaction> jsonDeserializer = new JsonDeserializer<>(Transaction.class);
+        jsonDeserializer.addTrustedPackages("com.jpmc.midascore.foundation");
+
+        return new DefaultKafkaConsumerFactory<>(
+                props,
+                new StringDeserializer(),
+                jsonDeserializer
+        );
+    }
+
+//    @Bean(name = "transactionKafkaListenerContainerFactory")
+//    public ConcurrentKafkaListenerContainerFactory<String,Transaction> transactionConcurrentKafkaListenerContainerFactory(){
+//        ConcurrentKafkaListenerContainerFactory<String, Transaction> factory =
+//                new ConcurrentKafkaListenerContainerFactory<>();
+//        factory.setConsumerFactory(transactionConsumerFactory());
+//
+//        return factory;
+//    }
+}
